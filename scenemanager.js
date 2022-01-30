173759c24013ecba8b7555007ed1e498ee0a043d
atienny <83434944+atienny@users.noreply.github.com>
2022-01-30 14:43:12 -0800

    Remove zombie

--- scenemanager.js
+++ scenemanager.js
@@ -4,11 +4,6 @@ class SceneManager {
         this.game.camera = this;
         this.x = 0;
         this.y = 0;
-
-        this.lyra = {x: 1295, y: 900};
-
-        this.zombie = {x: 1500, y: 950};
-
         this.lyra = {x: 0, y:0};
         this.loadLevel();
     };
@@ -16,18 +11,10 @@ class SceneManager {
     loadLevel() {
         
         this.loadLayer(level.floor);
-        this.loadLayer(level.wall);
-
-        this.lyra = new Lyra(this.game, 1295, 900, ASSET_MANAGER.getAsset("./sprites/character.png"));
-
-        this.zombie = new zombie(this.game, 1500, 950, ASSET_MANAGER.getAsset("./sprites/zombie.png"));
-
         this.loadLayer(level.wall_btm);
         this.lyra = new Lyra(this.game, 1045, 700, ASSET_MANAGER.getAsset("./sprites/character.png"));
         this.game.addEntity(this.lyra);
         this.loadLayer(level.wall_top);
-
-        this.game.addEntity(this.zombie);
 
     };
 
@@ -36,9 +23,6 @@ class SceneManager {
         let midpoint = { x : PARAMS.CANVAS_WIDTH / 2 - PARAMS.BLOCKWIDTH / 2, y : PARAMS.CANVAS_HEIGHT / 2 - PARAMS.BLOCKWIDTH / 2 };
         this.x = this.lyra.x - midpoint.x;
         this.y = this.lyra.y - midpoint.y;
-
-        //this.x = this.zombie.x - midpoint.x;
-        //this.y = this.zombie.y - midpoint.y;
     };
 
     draw(ctx) {
